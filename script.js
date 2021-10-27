0fbb617f67793643fe7a216d587c87d63a83c867
Oncon <62356024+OnconProger@users.noreply.github.com>
2021-10-27 21:21:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('button');
+let image = document.querySelector('img');
+
+button.addEventListener('click', function(){
+    swal({
+        content: image,
+        button: 'ДА ОНКОН ТРАХНИ МЕНЯ'
+    });
+})
